77da447b138f5531bfa0e8ee48a65271363d2d60
Douglas Floriano <douglas97floriano@gmail.com>
2026-06-05 16:16:42 -0300

    Add Typography component and update Main layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Aside } from './components/Asides';
 import { Container } from './components/Container';
 import { Main } from './components/Main';
 import { SearchInput } from './components/SearchInput';
+import { Typography } from './components/Typography';
 
 function App() {
     return (
@@ -9,7 +10,15 @@ function App() {
             <Container>
                 <Aside />
                 <Main>
-                    <SearchInput name='q' />
+                    <SearchInput name="q" />
+                    <div>
+                        <Typography variant="h1">
+                            Olá, Douglas!
+                        </Typography>
+                        <Typography>
+                            Veja como estão suas finanças hoje.
+                        </Typography>
+                    </div>
                 </Main>
             </Container>
         </div>
