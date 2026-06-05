f7ddf632cb4a741b81e6ae2556f969fe9f4563e8
Douglas Floriano <douglas97floriano@gmail.com>
2026-06-05 15:39:50 -0300

    add Container component and update App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { Aside } from "./components/Asides"
+import { Aside } from './components/Asides';
+import { Container } from './components/Container';
 
 function App() {
-
-  return (
-    <div className='min-h-screen bg-neutral-background'>
-      <Aside />
-    </div>
-  )
+    return (
+        <div className="min-h-screen bg-neutral-background">
+            <Container>
+                <Aside />
+            </Container>
+        </div>
+    );
 }
 
-export default App
+export default App;
