1130ac894d72234ef23bc6c8b72d458776da857e
Douglas Floriano <douglas97floriano@gmail.com>
2026-06-05 15:58:58 -0300

    add Main and SearchInput components, and search icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Aside } from './components/Asides';
 import { Container } from './components/Container';
+import { Main } from './components/Main';
+import { SearchInput } from './components/SearchInput';
 
 function App() {
     return (
         <div className="min-h-screen bg-neutral-background">
             <Container>
                 <Aside />
+                <Main>
+                    <SearchInput name='q' />
+                </Main>
             </Container>
         </div>
     );
